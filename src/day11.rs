1ff689b4a635341c0f29ad12e61a00434ba9bbb8
Sebastian Andersson <sebastian.andersson@lumenradio.com>
2024-12-11 09:33:36 +0100

    Use the memoize crate for day11

--- src/day11.rs
+++ src/day11.rs
@@ -4,8 +4,7 @@
 
 use aoc_runner_derive::{aoc, aoc_generator};
 
-use std::collections::HashMap;
-// use std::collections::LinkedList;
+use memoize::memoize;
 use std::str::FromStr;
 
 type InputType = SolutionType;
@@ -24,34 +23,25 @@ pub fn solve_part1(data: &[InputType]) -> SolutionType {
     data.iter().map(|n| count(*n, 25)).sum()
 }
 
-fn count_(mem: &mut HashMap<(InputType, u8), SolutionType>, n: InputType, gen: u8) -> SolutionType {
+#[memoize]
+fn count(n: InputType, gen: u8) -> SolutionType {
     if gen == 0 {
         return 1;
     }
-    if let Some(res) = mem.get(&(n, gen)) {
-        return *res;
-    }
-    let res = if n == 0 {
-        count_(mem, 1, gen - 1)
+
+    if n == 0 {
+        count(1, gen - 1)
     } else {
         let digits = n.ilog10() + 1;
         if digits % 2 == 0 {
             let pow = (10 as SolutionType).pow(digits / 2);
             let first = n / pow;
             let second = n % pow;
-            count_(mem, first, gen - 1) + count_(mem, second, gen - 1)
+            count(first, gen - 1) + count(second, gen - 1)
         } else {
-            count_(mem, n * 2024, gen - 1)
+            count(n * 2024, gen - 1)
         }
-    };
-    mem.insert((n, gen), res);
-    res
-}
-
-fn count(n: InputType, gen: u8) -> SolutionType {
-    let mut mem = HashMap::new();
-
-    count_(&mut mem, n, gen)
+    }
 }
 
 #[aoc(day11, part2)]
