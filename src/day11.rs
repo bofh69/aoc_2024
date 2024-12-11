eea176f1ed140642dcccccd72a464a6bd2b9b8fa
Sebastian Andersson <sebastian.andersson@lumenradio.com>
2024-12-11 08:14:19 +0100

    Day 11, use part2 for part1

--- src/day11.rs
+++ src/day11.rs
@@ -21,44 +21,7 @@ pub fn input_generator(input: &str) -> Vec<InputType> {
 
 #[aoc(day11, part1)]
 pub fn solve_part1(data: &[InputType]) -> SolutionType {
-    let mut data = data.to_vec();
-
-    let mut next = Vec::new();
-    for _ in 0..75 {
-        for &n in &data {
-            if n == 0 {
-                next.push(1);
-            } else {
-                let mut digits = 1;
-                let mut num = n;
-                while num >= 10 {
-                    num /= 10;
-                    digits += 1;
-                }
-                if digits % 2 == 0 {
-                    // Even numbers
-                    let mut first = n;
-                    let mut second = 0;
-                    let mut fac = 1;
-                    digits /= 2;
-                    while digits > 0 {
-                        second += (first % 10) * fac;
-                        first /= 10;
-                        fac *= 10;
-                        digits -= 1;
-                    }
-                    next.push(first);
-                    next.push(second);
-                } else {
-                    next.push(n * 2024);
-                }
-            }
-        }
-        (data, next) = (next, data);
-        next.clear();
-    }
-
-    data.iter().count() as SolutionType
+    data.iter().map(|n| count(*n, 25)).sum()
 }
 
 fn count_(mem: &mut HashMap<(InputType, u8), SolutionType>, n: InputType, gen: u8) -> SolutionType {
