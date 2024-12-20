4ed919d1eaf45d6eac50549aade60f959bb5a126
Sebastian Andersson <sebastian@bittr.nu>
2024-12-20 22:31:16 +0100

    Day 20, part 2 with rayon

--- src/day20.rs
+++ src/day20.rs
@@ -4,12 +4,13 @@
 
 use aoc_runner_derive::{aoc, aoc_generator};
 
-use ahash::{HashSet, HashSetExt};
-
 use advent_of_tools::*;
+
+use rayon::prelude::*;
 
 type InputType = Map;
 type SolutionType = usize;
+type StepType = u16;
 
 #[aoc_generator(day20)]
 pub fn input_generator(input: &str) -> InputType {
@@ -42,11 +43,11 @@ fn get_idx(map: &Map, pos: Point) -> usize {
     (pos.x + pos.y * map.get_width()) as usize
 }
 
-fn find_steps_from(map: &Map, from: Point) -> Vec<u16> {
+fn find_steps_from(map: &Map, from: Point) -> Vec<StepType> {
     let mut frontier = std::collections::VecDeque::new();
 
     let mut costs = Vec::new();
-    costs.resize((map.get_height() * map.get_width()) as usize, u16::MAX);
+    costs.resize((map.get_height() * map.get_width()) as usize, StepType::MAX);
     frontier.push_front((from, 0));
     while let Some((pos, steps)) = frontier.pop_back() {
         if !matches!(map.get_at_unchecked(pos), b'.' | b'S' | b'E') {
@@ -57,9 +58,6 @@ fn find_steps_from(map: &Map, from: Point) -> Vec<u16> {
             continue;
         }
         costs[idx] = steps;
-        // if map.get_at_unchecked(pos) == b'S' {
-        // println!("Start: pos={pos:?}, idx={idx}, steps={steps}");
-        // }
         for dir in [Dir::North, Dir::East, Dir::South, Dir::West] {
             let new_pos = pos.walk(dir);
             frontier.push_front((new_pos, steps + 1));
@@ -72,7 +70,7 @@ fn find_steps_from(map: &Map, from: Point) -> Vec<u16> {
             && pos.x < map.get_width()
             && pos.y < map.get_height()
         {
-            let mut cheapest = u16::MAX;
+            let mut cheapest = StepType::MAX;
             for (p, d, c) in map.neighbors(pos) {
                 if !d.is_cardinal() {
                     continue;
@@ -89,11 +87,19 @@ fn find_steps_from(map: &Map, from: Point) -> Vec<u16> {
     costs
 }
 
+fn get_steps_at(map: &Map, pos: Point, steps: &[StepType]) -> StepType {
+    let idx = get_idx(map, pos);
+    steps[idx]
+}
+
 #[aoc(day20, part2)]
 pub fn solve_part2(map: &InputType) -> SolutionType {
     /*
      * Cheats start and end at 'S' and '.' tiles and end at '.' and 'E' tiles.
      */
+
+    const MIN_GAIN: StepType = 100;
+    const MAX_CHEAT_LEN: i32 = 20;
 
     let start = map.find(b'S')[0];
     let end = map.find(b'E')[0];
@@ -101,78 +107,62 @@ pub fn solve_part2(map: &InputType) -> SolutionType {
     let steps_to_end = find_steps_from(map, end);
 
     let start_idx = get_idx(map, start);
-    let max_time = steps_to_end[start_idx] - 100;
+    let max_time = steps_to_end[start_idx] - MIN_GAIN;
 
-    let mut total_cheats = 0;
-    let mut cheats = HashSet::new();
-    for (pos, c) in map.iter() {
-        if matches!(c, b'.' | b'S')
-            && pos.x > 0
-            && pos.y > 0
-            && pos.x < map.get_width()
-            && pos.y < map.get_height()
-        {
-            let pos_idx = get_idx(map, pos);
-            let pos_steps = steps_from_start[pos_idx];
-            for d in [Dir::North, Dir::South, Dir::East, Dir::West] {
-                let new_pos = pos.walk(d);
-                let new_pos_idx = get_idx(map, new_pos);
-                if map.get_at_unchecked(new_pos) != b'#' {
+    map.iter()
+        .filter(|(pos, c)| {
+            matches!(c, b'.' | b'S')
+                && pos.x > 0
+                && pos.y > 0
+                && pos.x < map.get_width()
+                && pos.y < map.get_height()
+        })
+        .filter_map(|(pos, _c)| {
+            let pos_steps = get_steps_at(map, pos, &steps_from_start);
+
+            let m_d = pos.manhattan_distance(end);
+
+            if pos_steps + (m_d as StepType) > max_time {
+                return None;
+            }
+            Some((pos, pos_steps))
+        })
+        .collect::<Vec<_>>()
+        .par_iter()
+        .map(|(pos, pos_steps)| {
+            let mut total_cheats = 0;
+            for dy in -MAX_CHEAT_LEN..=MAX_CHEAT_LEN {
+                let y = dy + pos.y;
+                if y < 1 {
                     continue;
                 }
-                let new_steps = steps_from_start[new_pos_idx];
-
-                if new_steps != pos_steps + 1 {
+                if y >= map.get_height() - 1 {
                     continue;
                 }
-
-                let m_d = new_pos.manhattan_distance(end);
-
-                if new_steps + (m_d as u16) > max_time {
-                    continue;
-                }
-
-                for dy in -19..=19i32 {
-                    let y = dy + pos.y;
-                    if y < 1 {
+                for dx in -MAX_CHEAT_LEN..=MAX_CHEAT_LEN {
+                    let m_d = dx.abs() + dy.abs();
+                    if dx == 0 && dy == 0 || m_d > MAX_CHEAT_LEN {
                         continue;
                     }
-                    if y >= map.get_height() - 1 {
+                    let x = dx + pos.x;
+                    if x < 1 {
                         continue;
                     }
-                    for dx in -19..=19i32 {
-                        let m_d = (dx.abs() + dy.abs()) as u16;
-                        if dx == 0 && dy == 0 || m_d > 20 {
-                            continue;
-                        }
-                        let x = dx + pos.x;
-                        if x < 1 {
-                            continue;
-                        }
-                        if x >= map.get_width() - 1 {
-                            continue;
-                        }
-                        let cheat_to_pos = Point { x, y };
-                        let c = map.get_at_unchecked(cheat_to_pos);
-                        if b'.' == c || b'E' == c {
-                            let idx = (x + y * map.get_width()) as usize;
-                            let total_steps = pos_steps + m_d + steps_to_end[idx];
-                            if total_steps <= max_time {
-                                cheats.insert(cheat_to_pos);
-                                if pos.x == start.x && pos.y == start.y {
-                                    println!("Cheat to {x},{y}, total_steps = {total_steps}");
-                                }
-                            }
+                    if x >= map.get_width() - 1 {
+                        continue;
+                    }
+                    let cheat_to_pos = Point { x, y };
+                    let c = map.get_at_unchecked(cheat_to_pos);
+                    if matches!(c, b'.' | b'E') {
+                        let idx = (x + y * map.get_width()) as usize;
+                        let total_steps = pos_steps + m_d as StepType + steps_to_end[idx];
+                        if total_steps <= max_time {
+                            total_cheats += 1;
                         }
                     }
                 }
             }
-            total_cheats += cheats.len();
-            cheats.clear();
-        }
-    }
-
-    assert!(total_cheats > 880417, "{total_cheats} should be > 880417");
-
-    total_cheats
+            total_cheats
+        })
+        .sum()
 }
