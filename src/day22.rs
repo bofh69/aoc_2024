f736b961803a35cba04452580e624ab84287fa59
Sebastian Andersson <sebastian.andersson@lumenradio.com>
2024-12-22 16:46:32 +0100

    Optimized day 22, part 2

--- src/day22.rs
+++ src/day22.rs
@@ -23,6 +23,7 @@ pub fn input_generator(input: &str) -> InputType {
         .collect()
 }
 
+#[inline(always)]
 fn next(n: NumType) -> NumType {
     let n1 = ((n * 64) ^ n) & ((1 << 24) - 1);
     let n2 = ((n1 / 32) ^ n1) & ((1 << 24) - 1);
@@ -44,47 +45,34 @@ pub fn solve_part1(data: &InputType) -> SolutionType {
 
 #[aoc(day22, part2)]
 pub fn solve_part2(data: &InputType) -> SolutionType {
-    let prices: Vec<([i8; 2000], [i8; 2000])> = data
-        .iter()
-        .map(|&n| {
-            let mut n = n;
-            let mut prices = [0i8; 2000];
-            let mut price_diffs = [0i8; 2000];
-            for i in 0..2000 {
-                let old_price = (n % 10) as i8;
-                n = next(n);
-                prices[i] = (n % 10) as i8;
-                price_diffs[i] = (n % 10) as i8 - old_price;
-            }
-            (prices, price_diffs)
-        })
-        .collect();
-    let mut max_bananas = 0;
-    for i in -9..=9 {
-        println!("Checking {i}");
-        for j in -9..=9 {
-            for k in -9..=9 {
-                for l in -9..=9 {
-                    let mut bananas = 0;
-                    for monkey_prices in &prices {
-                        for m in 3..2000 {
-                            if monkey_prices.1[m - 3] == i
-                                && monkey_prices.1[m - 2] == j
-                                && monkey_prices.1[m - 1] == k
-                                && monkey_prices.1[m] == l
-                            {
-                                bananas += monkey_prices.0[m] as NumType;
-                                break;
-                            }
-                        }
-                    }
-                    if bananas > max_bananas {
-                        println!("For {i} {j} {k} {l}, got {bananas}");
-                    }
-                    max_bananas = max_bananas.max(bananas);
-                }
+    let mut prices = [0; 19*19*19*19];
+
+    data.iter().for_each(|&n| {
+        let mut n = n;
+        let old_price = n % 10;
+        n = next(n);
+        let price = n % 10;
+        let mut idx = (price - old_price + 9) as usize;
+        n = next(n);
+        let old_price = price;
+        let price = n % 10;
+        idx = idx * 19 + (price - old_price + 9) as usize;
+        n = next(n);
+        let old_price = price;
+        let mut price = n % 10;
+        idx = idx * 19 + (price - old_price + 9) as usize;
+
+        let mut first = [true; 19*19*19*19];
+        for _ in 3..2000 {
+            n = next(n);
+            let old_price = price;
+            price = n % 10;
+            idx = (idx * 19 + (price - old_price + 9) as usize) % (19*19*19*19);
+            if first[idx] {
+                prices[idx] += price;
+                first[idx] = false;
             }
         }
-    }
-    max_bananas
+    });
+    prices.iter().map(|n| *n).max().unwrap() as SolutionType
 }
